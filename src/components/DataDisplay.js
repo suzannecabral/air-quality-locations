a14a28bd3cec265c31c6b06217bf7c202a35f80a
Suzanne Cabral <suzannecabral@users.noreply.github.com>
2021-06-17 17:47:48 -0700

    Reorganize data display text

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -6,8 +6,13 @@ import React from 'react';
 const DataDisplay =(props)=> {
     return(
         <div className='data-display'>
-            <p>The Data Display component lives here</p>
-            <p role="alert" className="error">This is an error message</p>
+            <div className='data-description'>
+                <p>The Data Display component lives here</p>
+                <p role="alert" className="error">This is an error message</p>
+            </div>
+            <div class="data-list">
+
+            </div>
         </div>
     );
 }
