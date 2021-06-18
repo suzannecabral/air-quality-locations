7fc73c2c3813924771f2cee2b28b67741ae5e392
Suzanne Cabral <suzannecabral@users.noreply.github.com>
2021-06-18 11:28:09 -0700

    Add comments, new state for DataDisplay

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 // Needs communityData, error, errorMsg
@@ -8,9 +8,28 @@ const DataDisplay =(props)=> {
 
     const { communityData } = props;
 
+    const [displayData, setDisplayData] = useState([]);
+
+    const filterForDisplay = (arr) => {
+        // must have at least 1 measurement using micrograms/m3
+
+        // add keys: maxParamAvg | maxParamUnit | maxParamType | maxParamId
+        // take max measurement average: record measurement, unit and type
+        // maxParamId is the id of the measurement object, not the server's "parameterId"
+        /*{
+            communityName: Smallville
+            communityId: 1234
+            maxParamAvg: 0.065
+            maxMaramUnit: µg/m³
+            maxParamType: PM2.5
+            maxParamId: 5678
+        }*/
+    }
+
     useEffect(()=>{
-        console.log("Data display updated");
-        console.log("Data display props: ", props);
+        // transform community data before setting to display
+        setDisplayData([...communityData]);
+        console.log("Display Data: ", displayData);
     },[props]);
 
     return(
@@ -19,15 +38,17 @@ const DataDisplay =(props)=> {
             </div>
             <div className="data-list">
                 {
-                    communityData.map((obj, idx)=>{
-                    return (<div key={'obj'+idx} className='community'><p className="community-name">{obj.name}</p>  
-                        {
-                            obj.parameters.map((param,idx)=>{
-                                return(<p key={'param'+idx}  className={param.unit === "µg/m³" && param.average > 0 && param.average < 5 ? 'targetAQ' : ''}> { (Math.ceil(param.average * 1000)/1000).toFixed(3)+ ' ' + param.unit + ' ' + param.parameter}</p>);
-                            })
-                        }
-                    </div>)
-                    })
+                    
+
+                    // communityData.map((obj, idx)=>{
+                    // return (<div key={'obj'+idx} className='community'><p className="community-name">{obj.name}</p>  
+                    //     {
+                    //         obj.parameters.map((param,idx)=>{
+                    //             return(<p key={'param'+idx}  className={param.unit === "µg/m³" && param.average > 0 && param.average < 5 ? 'targetAQ' : ''}> { (Math.ceil(param.average * 1000)/1000).toFixed(3)+ ' ' + param.unit + ' ' + param.parameter}</p>);
+                    //         })
+                    //     }
+                    // </div>)
+                    // })
                 }
             </div>
         </div>
